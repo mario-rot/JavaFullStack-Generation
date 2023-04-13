5046caa4675593c4c634c1d783559791108430ba
Mario Rot <mario_netta12@gmail.com>
2023-04-12 21:59:15 -0600

    Adding the style to the second excercise of Tania's homework

--- Ejercicios/web/web2/PesonalPage/assets/js/index.js
+++ Ejercicios/web/web2/PesonalPage/assets/js/index.js
@@ -1,1 +1,17 @@
-console.log("Hola Mundo!")
+function changeGreeting(){
+  // get client name
+  const clientName =  getNameOfClient();
+  // Get reference label h1
+  const greetingReference = getReferenceOfIdGreeting();
+  // TODO Change the greeting
+  greetingReference.innerHTML = " -----------------------------------   Welcome dear: " + clientName + '   -----------------------------------';
+}
+
+function getNameOfClient(){
+  const clientName = prompt("Write your name here:");
+  return clientName
+}
+ 
+function getReferenceOfIdGreeting(){
+  return document.getElementById("greeting");
+}
--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,20 +1,17 @@
-console.log("Session web1");
-
 function changeGreeting(){
-  console.log("Button is pushed")
   // get client name
   const clientName =  getNameOfClient();
   // Get reference label h1
   const greetingReference = getReferenceOfIdGreeting();
   // TODO Change the greeting
-  greetingReference.innerHTML = "Hola " + clientName;
+  greetingReference.innerHTML = " -----------------------------------   Welcome dear: " + clientName + '   -----------------------------------';
 }
 
 function getNameOfClient(){
   const clientName = prompt("Write your name here:");
   return clientName
 }
-
+ 
 function getReferenceOfIdGreeting(){
   return document.getElementById("greeting");
 }
