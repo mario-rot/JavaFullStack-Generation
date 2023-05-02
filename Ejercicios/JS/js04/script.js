215a5b642721021ad872791705957633c42f7a41
Mario Rot <mario_netta12@gmail.com>
2023-05-02 12:41:59 -0600

    Finishing js04 and js05

--- Ejercicios/JS/js04/script.js
+++ Ejercicios/JS/js04/script.js
@@ -5,6 +5,7 @@ const nombresCh26 = ["Sofi", "Alejandro", "Ixchel", "Said"]
 // Usando el constructor de arreglos
 const apellidosCh26 = new Array("Montellano", "Torres", "Vazquez")
 const numeroDeCajonesEstacionameinto = new Array(4)
+const numCohortesActivas = new Array(26, 27, 28, 29); // [26 , 27 , 28 , 29 ]
 
 
 console.log("Apellido de Sergio: " + apellidosCh26[1])
@@ -13,6 +14,200 @@ console.log("Apellido de Sergio: " + apellidosCh26[1])
 apellidosCh26.push("Ruiz")
 console.log(apellidosCh26)
 
+// Matrices (arreglos de 2 dimensiones)
+// Arrays anidados
+// [ [] , [] , []  ]
 
+const personasEnCohorte = [
+/*ch26*/["Arath", "José", "Karen", "Sofi"],
+/*ch27*/["Manuel", "María", "Francisco", "Pablo", "Pepe"],
+/*ch28*/["Doris", "Brendan", "Lucas", "Maria"]
+];
+
+console.log("Personas ch27: " + personasEnCohorte[1].join(", "));
+
+//Accediento a Francisco
+// ["Manuel" , "María"  , "Francisco" , "Pablo", "Pepe" ]
+//const temporalCh27 = ["Manuel" , "María"  , "Francisco" , "Pablo", "Pepe" ];
+const temporalCh27 = personasEnCohorte[1];
+const paco = temporalCh27[2];
+console.log("Extraer en pasos " + paco); // Francisco
+console.log("Paco en Ch27 " + personasEnCohorte[1][2]); // Francisco
+console.log("Lucas de Ch28 " + personasEnCohorte[2][2]); // Lucas
+console.log("Karen de Ch26 " + personasEnCohorte[0][2]); // Karen
+
+personasEnCohorte[1][3] = "Juán";
+console.log("Personas ch27: " + personasEnCohorte[1].join(", "));
+
+// myArray3D = [ [ [],[],[]  ], [ [],[],[] ] ];  myArray3D[][][];
+
+// Iterar todos los elementos en un arreglo de 2 dimensiones.
+
+for (let fila = 0; fila < personasEnCohorte.length; fila++) {
+  console.log(`Indice ${fila}: ${personasEnCohorte[fila]}`)
+  for (let columna = 0; columna < personasEnCohorte[fila].length; columna++) {
+    console.log(`Nombre: ${personasEnCohorte[fila][columna]}`)
+
+  }
+}
+
+// Usando forEach
+personasEnCohorte.forEach(fila => fila.forEach(columna => console.log(columna)));
+
+// For of
+// Ejecuta sentencias por cada elemento de un objeto iterable(arrays, strings y objetos )
+
+const myName = "Sergio";
+for (let char of myName) {
+  console.log(char);
+}
+
+for (let fila of personasEnCohorte) {
+  for (let columna of fila) {
+    console.log(columna);
+  }
+}
+
+// -------------------- Consideraciones para el ciclo For -------------------------------
+// No es necesario indicar el inicio, la comparación y la expresion final
+/*
+Sintaxis ciclo For:
+   for (inicio; comparación; expresiónFinal  ){
+    sentencias;
+   }
+*/
+// Break: Rompe con la iteración en curso y sale del ciclo
+let nIteracion = 0;
+for ( /*inicio*/;  /*comparacion*/;  /*ExpFinal*/) {
+  console.log(nIteracion);
+  if (nIteracion === 5) break;
+  nIteracion++;
+}
+
+// -------------------- Ejercicio ---------------------------
+// Iterar y mostrar a las personas en personasEnCohorte y detener cuando se encuentre a María.
+// Dejar de iterar
+personas: // Se usa un label para indicar en el break hacia que ciclo romperá.
+for (let fila of personasEnCohorte) {
+  columna:
+  for (let columna of fila) {
+    console.log(columna);
+    if (columna === "María") break personas; // Se indica el label que rompe
+  }
+  //    if(columna == "María") break; // Si no se usa el label, se tiene que realizar esto
+}
+
+// Continue
+/*
+ Termina la ejecución de la sentencia en la iteración actual y continua la ejecución del ciclo
+ con la próxima iteración (o en el label que se indique).
+*/
+console.log("==== uso de continue======")
+// No imprimir a María
+personas:
+for (let fila of personasEnCohorte) {
+  for (let columna of fila) {
+    if (columna === "María" || columna === "Maria") continue;
+
+    const concatenacion = "Persona " + columna;
+    console.log(concatenacion);
+
+  }
+}
+
+// ------------------ Ciclo While ---------------------------------
+/*
+ Crea un bucle que ejecuta una sentencia mientras la condición especificada sea verdadera.
+
+ Sintaxis:
+
+while( condición )
+ sentencias;
+
+*/
+/*
+let counter = 0;
+while ( confirm("¿Quieres continuar?")  ){
+    console.log(`Has continuado ${++counter} ${counter>1? "veces":"vez"}`);
+} // counter++ vS. ++counter --> https://stackoverflow.com/a/30420784
+*/
+
+const numbers = [9, 5, 6, 7];
+//numbers.forEach( number => console.log(number));
+
+let i = 0;
+while (i < numbers.length) {
+  console.log(numbers[i],)
+  i++;
+}
+
+// ---------------- Práctica -----------------------
+/*
+ 
+
+*/
+
+// ------------- Ciclo do while --------------------
+/*
+ Crea un bucle que ejecuta una sentencia hasta que la condición de comprobación se evalue
+ como falso. La condición se evalua después de ejecutar la sentencia, por lo que la sentencia
+ se ejecuta por lo menos una vez.
+
+ Sintaxis:
+
+  do{
+    setencias;
+  } while( comprobación );
+
+
+
+*/
+let iteration = 0;
+/*
+do {
+    iteration++;
+    console.log("N. iteración do-while: " + iteration);
+
+} while( confirm("¿Deseas Continuar? ") );
+*/
+
+let valor = -2;
+while (valor < 5) {
+  console.log("While " + valor);
+  valor++;
+}
+
+calor = 5;
+valor = 5;
+do {
+  console.log("Do-While " + valor);
+  valor++;
+} while (valor < 5);
+
+/*
+FIFO: first input, first output
+LIFO: last input, first output
+
+*/
+const alimentos = [];
+
+// Usar FIFO:
+alimentos.push("Manzanas del lunes");
+alimentos.push("Manzanas del miércoles");
+console.log(alimentos);
+// Sacar las manzanas
+console.log("Sacando las manzanas antiguas: " + alimentos.shift());
+console.log(alimentos);
+console.log("Sacando las manzanas antiguas: " + alimentos.shift());
+console.log(alimentos);
+
+// Usar LIFO
+const productos = [];
+// Temporada navideña
+productos.push("Desarmadores");
+productos.push("Luces de colores");
+console.log(productos);
+console.log("Sacando el producto reciente: " + productos.pop());
+console.log(productos);
 
 
---
+++ Ejercicios/JS/js05/script.js
@@ -0,0 +1,244 @@
+console.log("Sesion js05 operadores");
+
+// ------------ Operadores aritméticos -----------
+// Adición +
+// Multiplicación *
+// Substracción -
+// División /
+// Exponencial **
+// Resto %
+// 
+
+
+// (1 + ((2 ** 3) * 4 / 5)) >> 6
+// │    │ └─ 1. ─┘        │ │
+// │    └────── 2. ───────┘ │
+// └────────── 3. ──────────┘
+
+
+// (1 + (((2 ** 3) * 4) / 5)) >> 6
+// │    │ │ └─ 1. ─┘     │    │ │
+// │    └─│─────── 2. ───│────┘ │
+// └──────│───── 3. ─────│──────┘
+//        └───── 4. ─────┘
+
+// ejemplo
+let dinero = 20 + 10; // 30
+
+// ------------- Operadores de asignación --------
+// Asigna un valor a su operando izquierdo basándose en el valor de
+// su operando derecho
+// Asignación =
+// Asignación de adición  +=
+dinero += 15;  // dinero = dinero + 15;
+// Multiplicación *=
+// Substracción -=
+// División /=
+// Exponencial **=
+// Resto %=
+12 + 7 + 12 + 4 + 1;
+
+2 ** 3 ** 2; // 512 (2^(3^2)) --> (2^9)
+
+// ------------- Operadores Unarios -------------------
+// Un operador unario realiza una operación en un solo operando.
+// Negación -
+dinero = - 3; // -3      // -= 3 -> dinero= dinero - 3
+console.log(dinero);
+// Conversión a Numérico +
+dinero = +dinero; // Sigue siendo -3
+dinero = + "200"; // 200 numérico
+console.log(dinero);
+
+// typeof 
+// Operador lógico not   !
+let valorBooleano = !true;
+console.log(valorBooleano);
+
+// Operadores de incremento y decremento en una unidad
+// Operador de pre-incremento y pre-decremento
+// pre-incremento ++valor
+// pre-decremento --valor
+
+// Operador de  post-incremento y post-decremento
+// post-incremento valor++
+// post-decremento valor--
+
+let numero = 10;
+numero++; // 11 devuelve el dato como antes de incrementar y despues incrementa
+++numero; // 12 perimero incrementa y luego devuelve el valor ya incrementado
+console.log(numero); // 12
+
+let nuevoValor = numero++;
+/*
+  let nuevoValor = numero;
+  numero = numero + 1;
+*/
+console.log("nuevo valor", nuevoValor); //12
+console.log("numero", numero) // 13
+
+let nuevoDato = ++numero;
+/*
+ numero = numero + 1;
+ nuevoDato = numero;
+
+*/
+console.log("nuevoDato", nuevoDato); // 14
+console.log("numero", numero); //14
+
+let x = 3;
+let y = ++x;
+console.log(`x: ${x++} y: ${++y}`); // 4 - 5
+console.log(`x: ${x} y: ${y}`);  // 5 - 5
+
+// ---------------- Operadores lógicos AND && y OR ||
+
+const a = true;
+const b = false;
+const c = true;
+console.log(a || b && c); // true
+
+// Operadores de corto circuito
+// Cuando los operadores lógicos AND y OR actuan sobre operandos diferentes a booleanos.
+
+// OP1 && OP 2    Si OP1 es verdadero, se realiza el resultado de OP2.
+console.log(a && "Holi Crayoli");
+console.log(b && "Holi Crayoli");
+console.log(NaN && "El valor es un número que se puede procesar") // NaN
+console.log("false" && "El valor es false"); // El valor es false
+console.log("" && "No hay dato"); // ""
+
+// OP1 || OP2   si OP1 es verdadero, se realiza el resultado de OP1.
+console.log(a || "Holi Crayoli"); // true
+console.log(b || "Holi Crayoli"); // "Holi Crayoli"
+console.log(NaN || "El valor es un número que se puede procesar") // "El valor..."
+console.log("false" || "El valor es false"); // "false"
+console.log("" || "No hay dato"); // "No hay dato"
+
+const noDefinido = true;
+console.log(noDefinido || theBigBang);
+
+// Quiero imprimir "Estás Autorizado" si la variable isOnline y isActive son true.
+const isOnline = true;
+const isActive = true;
+if (isOnline)
+  if (isActive)
+    console.log("Estás Autorizado");
+
+if (isOnline && isActive)
+  console.log('Estas autorizado')
+
+console.log(isOnline && isActive ? "Estas autorizado" : "No estas autorizado");
+
+console.log(isOnline && isActive && "Estas autorizado");
+
+// --------------------- Operadores relacionales ---------------------------
+/*
+Un operador de comparación compara sus operandos y devuelve un valor lógico en función de si la comparación es verdadera (true) o falsa (false). Los operandos pueden ser valores numéricos, de cadena, lógicos u objetos. Las cadenas se comparan según el orden lexicográfico estándar, utilizando valores Unicode. En la mayoría de los casos, si los dos operandos no son del mismo tipo, JavaScript intenta convertirlos a un tipo apropiado para la comparación. Este comportamiento generalmente resulta en comparar los operandos numéricamente.
+
+< menor que
+> mayor que
+<= menor o igual que
+>= mayor o igual que
+== igual que
+
+*/
+
+console.log(true == "true"); // false
+console.log(23 < "40"); // true
+
+console.log("b" < "A"); // false ( 98  < 65 )
+
+console.log("Hola" < "HOLA"); // false
+
+console.log("Hola" < "123"); // false 
+//  H: 72     1: 49
+
+console.log("Hola" < "KeHace" + 1000); // true 
+// "Hola" < "KeHace1000"  
+
+console.log("23" == 23); // true
+
+// ------------------- Comparación estricta ------------------
+/*
+ === estrictamente igual, debe ser igual en el valor y tipo
+ !== estrictamente diferente, debe ser difente en valor y tipo
+
+*/
+
+console.log("23" === 23); // false;
+console.log("true" === true); // false;
+console.log("45" !== 45) // true
+
+
+// Operador se asignación =
+const myNumber = 34;
+
+// Desestructuración (Destructuring)
+/*
+ Es una expresión en JS que hace posible extraer datos de arreglos o objetos y nos
+ ayuda a realizar asignaciones más complejas.
+*/
+let dataA = 10;
+let dataB = 20;
+console.log(`a: ${dataA} , b:${dataB} `);
+// Quiero intercambiar los datos
+/*
+let temporal;
+temporal = dataA;
+dataA = dataB;
+dataB = temporal;
+*/
+[dataA, dataB] = [dataB, dataA];
+console.log(`a: ${dataA} , b:${dataB} `);
+
+const calificaciones = [50, 80, 100, 30, 60, 99];
+// dataA = calificaciones[0]; // 50
+// dataB = calificaciones[1]; // 80
+let resto; // rest parameter
+[dataA, , , dataB, ...resto] = calificaciones;
+console.log(`a: ${dataA} , b:${dataB} `);
+console.log(resto);
+
+const primerAnio = {
+  dataA: "43 alumnos",
+  dataB: "50 alumnos",
+  dataC: "20 alumnos"
+};
+({ dataA, dataB } = primerAnio);
+
+console.log(`a: ${dataA} , b:${dataB} `);
+
+const auto = {
+  model: "Chevrolet",
+  year: 2023,
+  color: "red",
+  motor: "2.0"
+}
+
+console.log("Modelo " + auto.model); //auto["model"]
+
+// Asignando el calor model del objeto auto a la constante model 
+// Inicializar con un valor por default
+// const { model, colorAuto = "Pistache" } = auto;
+// console.log("Modelo " + model + " color " + colorAuto)
+
+// Asignando el calor model del objeto del auto a la constante model
+// Asignar un nuevo nombre al atributo
+const { model, color: colorAuto } = auto;
+console.log("Modelo " + model + " color " + colorAuto)
+
+
+// // ------------------  ejercicio ------------------------
+// /*
+//    Hacer una función que realice la división de 2 números
+//    y me entregue (return) en entero el cociente y residuo
+// */
+
+function division(a, b) {
+  return [parseInt(a / b), a % b]
+}
+
+console.log(`El resultado de 9/5, cociente: ${division(9, 5)[0]}, residuo ${division(9, 5)[1]} `);
+
+
