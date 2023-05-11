6be6e0be6bb0d6d78d065b7b62f3dbdae54e29c5
Mario Rot <mario_netta12@gmail.com>
2023-05-11 14:35:44 -0600

    First steps on JAVA

---
+++ Ejercicios/Java/HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo {
+  public static void main(String args []){
+    // Este es un comentario de linea
+    /*
+     * Este es un comentario de bloque
+     * A continuacion se enviara un mensaje en la consola
+     * */
+    System.out.println("Hola Mundo!");
+    
+  }
+
+}
