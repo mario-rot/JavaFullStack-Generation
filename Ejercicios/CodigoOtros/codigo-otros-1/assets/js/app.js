0712882f618ad65d4a8489d149560b2600b26fc0
Mario Rot <mario_netta12@gmail.com>
2023-05-12 11:00:21 -0600

    Adding CodigoOtros exercise

--- Ejercicios/CodigoOtros/codigo-otros-1/assets/js/app.js
+++ Ejercicios/CodigoOtros/codigo-otros-1/assets/js/app.js
@@ -1,3 +1,10 @@
+/*
+* This file establishes a connection with a Github API to extract 
+* information from a user using the Async-Await technique to make 
+* the GET, establishing also a catch in case some error occurs in 
+* the request of the information.
+* */
+
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
 const $name = document.getElementById('name'); // Getting element by ID
@@ -7,7 +14,8 @@ const $location = document.getElementById('location');// Getting element by ID
 async function displayUser(username) {
   $name.innerHTML = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  const data = await response.json() // Turning response into an object and assigning it to a data variable
+  // Turning response into an object and assigning it to a data variable
+  const data = await response.json()
   $name.textContent = `${data.name}`;
   $blog.textContent = `${data.blog}`;
   $location.textContent = `${data.location}`;
